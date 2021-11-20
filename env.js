06762f591e3bb6d24951e70ff35e73bd3869b7b0
CryptoMaster2000 <94725263+CryptoMaster2000@users.noreply.github.com>
2021-11-19 21:49:20 -0300

    Create env.js

---
+++ env.js
@@ -0,0 +1,12 @@
+const PRIVATE_KEY='Your wallet private key here'
+
+const TOKEN_ADDRESS='0x2859e4544c4bb03966803b044a93563bd2d0dd4d'; // change to Token address you want to target, in this case: "Binance-Peg SHIBA INU Token (SHIB)"
+const AMOUNT=0.01
+const LEVEL=0.1
+
+module.exports = {
+    PRIVATE_KEY,
+    TOKEN_ADDRESS,
+    AMOUNT,
+    LEVEL
+};
