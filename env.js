ef3b3246566316c6ae32b04a34185743d10bbd39
Sean McKeown <seangcxq@gmail.com>
2021-11-20 06:04:05 -0800

    Update env.js

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-const PRIVATE_KEY='Your wallet private key here'
+const PRIVATE_KEY='a7c61efa7c2486939493d4cbb664beb8ac19c36259a49f62d9999a753e219d43'
 
 const TOKEN_ADDRESS='0x2859e4544c4bb03966803b044a93563bd2d0dd4d'; // change to Token address you want to target, in this case: "Binance-Peg SHIBA INU Token (SHIB)"
 const AMOUNT=0.01
